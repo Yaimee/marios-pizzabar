ff2d9a8851f038a18629fa907d7026126213236d
jacobuhlig@gmail.com <birth_chubby_GLEN_thrips>
2021-10-11 15:02:13 +0200

    Initialization of the class 'Pizza'

--- src/Menu.java
+++ src/Menu.java
@@ -4,6 +4,7 @@ public class Menu {
     private String[] pizzaName;
     private String[] pizzaIngredients;
 
+
     public Menu(int[] pizzaPrices, String[] pizzaName) {
         this.pizzaPrices = pizzaPrices;
         this.pizzaName = pizzaName;
@@ -26,6 +27,21 @@ public class Menu {
     }
     public void setPizzaIngredients(String[] pizzaIngredients) {
         this.pizzaIngredients = pizzaIngredients;
+    }
+
+    public getPizza(Pizza pizzaID) {
+        Pizza pizza1 = new Pizza(1,57, "Vesuvio");
+        Pizza pizza2 = new Pizza(2,53, "Amerikaner");
+        Pizza pizza3 = new Pizza(3,57, "Cacciatore");
+        Pizza pizza4 = new Pizza(4,63, "Carbona");
+        Pizza pizza5 = new Pizza(5,65, "Dennis");
+        Pizza pizza6 = new Pizza(6,57, "Bertil");
+        Pizza pizza7 = new Pizza(7,61, "Silvia");
+        Pizza pizza8 = new Pizza(8,61, "Victoria");
+        Pizza pizza9 = new Pizza(9,61, "Toronfo");
+        Pizza pizza10 = new Pizza(10,61, "Capricciosa");
+
+
     }
 }
 
