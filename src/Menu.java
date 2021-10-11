4affcc4aaff80913ac320dc07bec0da8bab242ae
Healsu <timmie2500@gmail.com>
2021-10-11 14:45:16 +0200

    Tilføjet På Menu - Timmie

--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,40 @@
 public class Menu {
+    private int[] pizzaPrices;
+    private String[] pizzaName;
+    private String[] pizzaIngredients;
+
+    public Menu(int[] pizzaPrices, String[] pizzaName) {
+        this.pizzaPrices = pizzaPrices;
+        this.pizzaName = pizzaName;
+    }
+
+    public int[] getPizzaPrice() {
+        return pizzaPrices;
+    }
+
+    public void setPizzaPrice(int[] pizzaPrice) {
+        this.pizzaPrices = pizzaPrice;
+    }
+
+    public String[] getPizzaName() {
+        return pizzaName;
+    }
+
+    public void setPizzaName(String[] pizzaName) {
+        this.pizzaName = pizzaName;
+    }
+
+    public String[] getPizzaIngredients() {
+        return pizzaIngredients;
+    }
+
+    public void setPizzaIngredients(String[] pizzaIngredients) {
+        this.pizzaIngredients = pizzaIngredients;
+    }
 }
+
+
+
+
+
+
