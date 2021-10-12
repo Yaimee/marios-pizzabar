0f28967f3901c4144f8f83a76ea37fa8f7e96582
balt0012 <balt0012@stud.kea.dk>
2021-10-12 11:27:53 +0200

    main rigtig version

--- src/Main.java
+++ src/Main.java
@@ -18,13 +18,48 @@ public class Main {
             System.out.println("The menu is: ");
             for(int i = 1; i <= 10; i++) {
                 System.out.println(Menu.getPizzaNumber(i));
+                System.out.println("hej");
             }
         }
     }
 
     static public void alfonso() {
-        System.out.println("You've chosen Alfonso. What would you like to do?\n1. create new order\n");
+        System.out.println("You've chosen Alfonso.\n What would you like to do?\n1. create new order\n");
+        int select = scan.nextInt();
+        if (select == 1){
+            Order order = createNewOrder();
+        }
+    }
 
+    static public Order createNewOrder(){
+        Scanner scanner = new Scanner(System.in);
+        Pizza[] pizzas = new Pizza[0];
+        int pizzaId;
+        int minimumTime = 0;
+        do {
+            System.out.println("witch pizza do you want to add (0 for no more pizzas)");
+            if (pizzaId == 0){
+                break;
+            }
+            pizzaId = scanner.nextInt();
+            Pizza pizza = Menu.getPizzaNumber(pizzaId);
+            Pizza[] newPizzas = new Pizza[pizzas.length+1];
+            for (int i = 0; i < pizzas.length; i++) {
+                newPizzas[i] = pizzas[i];
+            }
+            newPizzas[newPizzas.length-1] = pizza;
+            pizzas = newPizzas;
+        } while (pizzaId != 0);
+        for (int i = 0; i < Order.getActiveOrders().length; i++) {
+            minimumTime = minimumTime + Order.getActiveOrders()[i].getPizzas().length;
+        }
+        System.out.println("what time do you want your order picked up? (minimum: " + minimumTime +")");
+        int pickUpTime = scanner.nextInt();
+        if (pickUpTime < minimumTime){
+            pickUpTime = minimumTime;
+        }
+        int orderNr = Order.getActiveOrders().length + Order.getCompletedOrders().length;
+        return new Order(pizzas, pickUpTime, orderNr);
     }
 
     public static void main(String[] args) {
