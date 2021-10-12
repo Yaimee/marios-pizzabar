6399e7687e8e7ef2231d20646cb46dd7e93dd6e9
jacobuhlig@gmail.com <birth_chubby_GLEN_thrips>
2021-10-12 11:08:51 +0200

    Initialization of the class 'Pizza'

--- src/Menu.java
+++ src/Menu.java
@@ -1,5 +1,5 @@
 public class Menu {
-    //Code by Timmie
+    //Code by Timmie & Jacob - start
 
     public static int[] getPizzaID() {
         return new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
@@ -19,22 +19,7 @@ public class Menu {
 
         return new Pizza(getPizzaID()[pizzaNumber-1], getPizzaPrices()[pizzaNumber-1], getPizzaName()[pizzaNumber-1]);
     }
-
-
-    public static Pizza[] getPizzaArray() {
-
-        Pizza[] pizzaMenu = new Pizza[10];
-
-        for (int i = 0; i <= 9; i++) {
-            pizzaMenu[i] = new Pizza(getPizzaID()[i], getPizzaPrices()[i], getPizzaName()[i]);
-
-        }
-
-        return pizzaMenu;
-
-    }
-
-
+    //Code by Timmie & Jacob - end
 }
 
 
