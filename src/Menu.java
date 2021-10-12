5c58a7953f37d6b6f536eafdf13cb26f475b9417
Healsu <timmie2500@gmail.com>
2021-10-12 10:32:25 +0200

    Tilføjet På Menu - Timmie

--- src/Menu.java
+++ src/Menu.java
@@ -1,29 +1,30 @@
 public class Menu {
     //Code by Timmie
 
-    private int[] getPizzaID() {
+    public static int[] getPizzaID() {
         return new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     }
 
-    private int[] getPizzaPrices() {
+    public static int[] getPizzaPrices() {
         return new int[]{57, 53, 57, 63, 65, 57, 61, 61, 61, 61};
     }
 
-    private String[] getPizzaName() {
+    public static String[] getPizzaName() {
         return new String[]{"Vesuvio", "Amerikaner", "Cacciatore", "Carbona", "Dennis",
                 "Bertil", "Silvia", "Victoria", "Toronfo", "Capricciosa"};
     }
 
 
-    public Pizza getPizzaNumber(int pizzaNumber) {
+    public static Pizza getPizzaNumber(int pizzaNumber) {
 
         Pizza[] pizzaMenu = new Pizza[10];
 
         for (int i = 0; i < 9; i++) {
             pizzaMenu[i] = new Pizza(getPizzaID()[i], getPizzaPrices()[i], getPizzaName()[i]);
+
         }
 
-        return pizzaMenu[pizzaNumber];
+        return pizzaMenu[pizzaNumber-1];
 
     }
 }
